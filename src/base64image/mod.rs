fdd665ae5eb21887f86904b89684817d691d7bad
Martin Kunz <martinkunz@email.cz>
2021-05-22 13:16:25 +0200

    add: test serialize base64image

--- src/base64image/mod.rs
+++ src/base64image/mod.rs
@@ -33,14 +33,28 @@ impl Serialize for Base64Image {
 mod test {
     use super::Base64Image;
     use mime::IMAGE_JPEG;
+    use serde_json::json;
 
     #[test]
     fn base64image_to_string() {
+        //{{{
         let base64image = Base64Image {
             img_path: "./test/assets/test_encode_base64.jpg".into(),
             img_mime: IMAGE_JPEG,
         };
         let string = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/wAALCAACAAIBAREA/8QAFAABAAAAAAAAAAAAAAAAAAAACP/EABwQAAEFAQEBAAAAAAAAAAAAAAIBAwQFBgcIAP/aAAgBAQAAPwBfeevPXAt7wLmm63XD+f6PSaPH01tcXFtmYUydZTpEJp1+TIfdbJx55xwzM3DJSIiUlVVVV+//2Q==".to_string();
         assert_eq!(base64image.to_string(), string);
-    }
+    } //}}}
+
+    #[test]
+    fn serialize_base64image() {
+        //{{{
+        let base64image = Base64Image {
+            img_path: "./test/assets/test_encode_base64.jpg".into(),
+            img_mime: IMAGE_JPEG,
+        };
+        let serialized = serde_json::to_value(&base64image).unwrap();
+        let acctual = json!("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/wAALCAACAAIBAREA/8QAFAABAAAAAAAAAAAAAAAAAAAACP/EABwQAAEFAQEBAAAAAAAAAAAAAAIBAwQFBgcIAP/aAAgBAQAAPwBfeevPXAt7wLmm63XD+f6PSaPH01tcXFtmYUydZTpEJp1+TIfdbJx55xwzM3DJSIiUlVVVV+//2Q==");
+        assert_eq!(serialized, acctual);
+    } //}}}
 }
