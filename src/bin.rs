676fa0723265b1cf20c53f9ad3cd4f8586b70380
Martin Kunz <martinkunz@email.cz>
2021-05-28 20:07:24 +0200

    add: start of clap implementation
    [skip ci]

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,26 @@
 extern crate mathpixapi;
 
-use clap::{App, Arg};
+#[cfg(clap)]
+use clap::{App, Arg, crate_version};
 
-fn main() {}
+#[cfg(clap)]
+fn main() {
+    let args = App::new("MathpixCLI")
+        .version(crate_version!())
+        .author("Martin Kunz <martinkunz@email.cz")
+        .about("Provides a command line interface for the Mathpix OCR API")
+        .subcommand(
+            App::new("text")
+                .about("Text endpoint for for the Mathpix API")
+                .arg(
+                    Arg::new("formats")
+                        .short('f')
+                        .long("formats")
+                        .about("List of formats from `text`, `data`, `html` and `latex_styled`")
+                        .value_name("FORMAT")
+                        .multiple(true)
+                        .takes_value(true),
+                ),
+        )
+        .get_matches();
+}
