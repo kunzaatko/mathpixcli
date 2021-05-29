62baee498921e0f867a32ada9c38cf6d6deb9fd6
Martin Kunz <martinkunz@email.cz>
2021-05-29 16:32:27 +0200

    edit: rename arguments to represent the fields in the data structure

--- src/bin.rs
+++ src/bin.rs
@@ -7,7 +7,8 @@ fn main() {
     let text_subcommand = App::new("text")
                 .about("Text endpoint for for the Mathpix API")
                 .arg(
-                    Arg::new("Text Formats")
+                    // TextBodyOptions.text_formats {{{
+                    Arg::new("TextBodyOptions.text_formats")
                         .short('f')
                         .long("format")
                         // TODO: What is the default <29-05-21, kunzaatko> //
@@ -16,9 +17,10 @@ fn main() {
                         .possible_values(&["text", "html", "data", "latex_styled", "all"])
                         .multiple(true)
                         .takes_value(true),
-                )
+                )//}}}
                 .arg(
-                    Arg::new("Data Options")
+                    // TextBodyOptions.data_options {{{
+                    Arg::new("TextBodyOptions.data_options")
                         .long("data_options")
                         // TODO: What is the default <29-05-21, kunzaatko> //
                         .about("List of data options for the outputs")
@@ -26,15 +28,17 @@ fn main() {
                         .possible_values(&["include_svg", "include_table_html", "include_latex", "include_tsv", "include_acsiimath", "include_mathml", "all"])
                         .multiple(true)
                         .takes_value(true),
-                )
+                )//}}}
                 .arg(
-                    Arg::new("Include Detected Alphabets")
+                    // TextBodyOptions.include_detected_alphabets {{{
+                    Arg::new("TextBodyOptions.include_detected_alphabets")
                         // TODO: What is the default <29-05-21, kunzaatko> //
                         .about("Return detected alphabets")
                         .long("include_detected_alphabets")
-                )
+                )//}}}
                 .arg(
-                    Arg::new("Alphabets allowed")
+                    // TextBodyOptions.alphabets_allowed {{{
+                    Arg::new("TextBodyOptions.alphabets_allowed")
                         // TODO: What is the default <29-05-21, kunzaatko> //
                         .about("Specify which alphabets are allowed for the OCR")
                         .long("alphabets_allowed")
@@ -42,66 +46,73 @@ fn main() {
                         .takes_value(true)
                         .multiple(true)
                         .possible_values(&["en","hi","zh","ja","ko","ru","th","all"])
-                )
-
-                // Confidence threshholds
+                )//}}}
                 .arg(
-                    Arg::new("Confidence Threshold")
+                    // TextBodyOptions.confidence_threshold {{{
+                    Arg::new("TextBodyOptions.confidence_threshold")
                         .about("Specifies threshold for triggering confidence errors")
                         .long("confidence_threshold")
                         .takes_value(true)
                         .value_name("THRESHOLD")
-                )
+                )//}}}
                 .arg(
-                    Arg::new("Confidence Rate Threshold")
+                    // TextBodyOptions.confidence_rate_threshold {{{
+                    Arg::new("TextBodyOptions.confidence_rate_threshold")
                         // NOTE: 0.75 is API default (no need to set as explicit default)
                         .about("Specifies threshold for triggering confidence errors for individual symbols [default: 0.75]")
                         .long("confidence_rate_threshold")
                         .takes_value(true)
                         .value_name("THRESHOLD")
-                )
-
-                // Included data formats
-                .group(ArgGroup::new("Included data formats")
+                )//}}}
+                .group(
+                    //  Included data formats {{{
+                    ArgGroup::new("Included data formats")
                         // TODO: What is the default <29-05-21, kunzaatko> //
-                        .arg("include_line_data")
-                        .arg("include_word_data")
-                        .arg("include_smiles")
-                        .arg("include_inchi")
-                        .arg("include_geometry_data")
+                        .arg("TextBodyOptions.include_line_data")
+                        .arg("TextBodyOptions.include_word_data")
+                        .arg("TextBodyOptions.include_smiles")
+                        .arg("TextBodyOptions.include_inchi")
+                        .arg("TextBodyOptions.include_geometry_data")
                         .multiple(true)
-                )
+                )//}}}
                 .arg(
-                    Arg::new("include_line_data")
+                    // TextBodyOptions.include_line_data {{{
+                    Arg::new("TextBodyOptions.include_line_data")
                         .long("include_line_data")
                         .about("Include the line data objects in the response")
-                )
+                )//}}}
                 .arg(
-                    Arg::new("include_word_data")
+                    // TextBodyOptions.include_word_data {{{
+                    Arg::new("TextBodyOptions.include_word_data")
                         .long("include_word_data")
                         .about("Include the word data objects in the response")
-                )
+                )//}}}
                 .arg(
-                    Arg::new("include_smiles")
+                    // TextBodyOptions.include_smiles {{{
+                    Arg::new("TextBodyOptions.include_smiles")
                         .long("include_smiles")
                         .about("Include the smiles data objects in the response")
-                )
+                )//}}}
                 .arg(
-                    Arg::new("include_inchi")
+                    // TextBodyOptions.include_inchi {{{
+                    Arg::new("TextBodyOptions.include_inchi")
                         .long("include_inchi")
                         .about("Include the InChI data as XML attributes inside smiles element")
-                )
+                )//}}}
                 .arg(
-                    Arg::new("include_geometry_data")
+                    // TextBodyOptions.include_geometry_data {{{
+                    Arg::new("TextBodyOptions.include_geometry_data")
                         .long("include_geometry_data")
                         .about("Include data extraction for geometry diagrams")
                 ); //}}}
+                   //}}}
 
     // LaTeX endpoint {{{
     let latex_subcommand = App::new("latex")
         .about("LaTeX endpoint for for the Mathpix API")
         .arg(
-            Arg::new("LaTeX formats")
+            // LaTeXBodyOptions.format_options {{{
+            Arg::new("LaTeXBodyOptions.formats")
                 .long("format")
                 .short('f')
                 // TODO: What is the default <29-05-21, kunzaatko> //
@@ -119,11 +130,11 @@ fn main() {
                     "all",
                 ])
                 .multiple(true)
-                .takes_value(true)
                 .required(true),
-        )
+        ) //}}}
         .arg(
-            Arg::new("OCR")
+            // LaTeXBodyOptions.ocr {{{
+            Arg::new("LaTeXBodyOptions.ocr")
                 .long("ocr")
                 // TODO: What is the default <29-05-21, kunzaatko> //
                 .about("Whether to process only `math` or both `math` and `text`")
@@ -131,16 +142,18 @@ fn main() {
                 .value_name("OCR")
                 .takes_value(true)
                 .multiple(true),
-        )
+        ) //}}}
         .group(
-            ArgGroup::new("Format Options")
-                .arg("Transforms")
-                .arg("Math delimiters")
-                .arg("Displayed math delimiters")
+            // LaTeXBodyOptions.format_options {{{
+            ArgGroup::new("LaTeXBodyOptions.format_options")
+                .arg("LaTeXBodyOptions.format_options.transforms")
+                .arg("LaTeXBodyOptions.format_options.math_delimiters")
+                .arg("LaTeXBodyOptions.format_options.displaymath_delims")
                 .multiple(true),
-        )
+        ) //}}}
         .arg(
-            Arg::new("Transforms")
+            // LaTeXBodyOptions.format_options.transforms {{{
+            Arg::new("LaTeXBodyOptions.format_options.transforms")
                 .long("transforms")
                 // TODO: What is the default <29-05-21, kunzaatko> //
                 .about("Transformations to be applied to the output")
@@ -155,95 +168,107 @@ fn main() {
                 ])
                 .value_name("TRANSFORM")
                 .multiple(true),
-        )
+        ) //}}}
         .arg(
-            Arg::new("Math delimiters")
+            // LaTeXBodyOptions.format_options.math_delimiters {{{
+            Arg::new("LaTeXBodyOptions.format_options.math_delimiters")
                 .long("math_delims")
                 // TODO: What is the default <29-05-21, kunzaatko> //
-                .about("`begin`,`end` delimiter to be used in inline math")
+                .about("delimiters to be used in inline math")
                 .number_of_values(2)
                 .value_names(&["LDELIM", "RDELIM"])
                 .multiple(true),
-        )
+        ) //}}}
+        // TODO: Add aliases <29-05-21, kunzaatko> //
         .arg(
-            Arg::new("Displayed math delimiters")
+            // LaTeXBodyOptions.format_options.displaymath_delims {{{
+            Arg::new("LaTeXBodyOptions.format_options.displaymath_delims")
                 .long("displaymath_delims")
                 // TODO: What is the default <29-05-21, kunzaatko> //
-                .about("`begin`,`end` delimiter to be used in displayed math")
+                .about("delimiters to be used in displayed math")
                 .value_names(&["LDELIM", "RDELIM"])
                 .number_of_values(2),
-        )
+        ) //}}}
         .arg(
-            Arg::new("Skip recrop")
+            // LaTeXBodyOptions.skip_recrop {{{
+            Arg::new("LaTeXBodyOptions.skip_recrop")
                 .long("skip_recrop")
-                .about("Skip the recroping before OCR for the image"),
-        )
+                .about("skip the recroping before OCR of the image"),
+        ) //}}}
         .arg(
-            Arg::new("Confidence threshold")
+            // LaTeXBodyOptions.confidence_threshold {{{
+            Arg::new("LaTeXBodyOptions.confidence_threshold")
                 .long("confidence_threshold")
                 // TODO: What is the default <29-05-21, kunzaatko> //
-                .about("Threshold for triggering confidence errors")
+                .about("threshold for triggering confidence errors")
                 .value_name("NUMBER"),
-        )
+        ) //}}}
         .arg(
-            Arg::new("Beam size")
+            // LaTeXBodyOptions.beam_size {{{
+            Arg::new("LaTeXBodyOptions.beam_size")
                 .long("beam_size")
                 // TODO: What is default <29-05-21, kunzaatko> //
-                .about("Number of results to consider during recognition")
+                .about("number of results to consider during recognition")
                 .value_name("NUMBER"),
-        )
+        ) //}}}
         .arg(
-            Arg::new("N best")
+            // LaTeXBodyOptions.n_best {{{
+            Arg::new("LaTeXBodyOptions.n_best")
                 .long("n_best")
                 // TODO: What is default <29-05-21, kunzaatko> //
-                .about("Number of best results to return")
+                .about("number of best results to return")
                 .value_name("NUMBER"),
-        )
+        ) //}}}
         // TODO: Special from string for region <29-05-21, kunzaatko> //
         .arg(
-            Arg::new("Region")
+            // LaTeXBodyOptions.region {{{
+            Arg::new("LaTeXBodyOptions.region")
                 .long("region")
-                .about("Region of the image to process")
+                .about("region of the image to process")
                 .number_of_values(4)
                 .value_names(&["TOP_LEFT_X", "TOP_LEFT_Y", "WIDTH", "HEIGHT"]),
-        )
+        ) //}}}
         .group(
-            ArgGroup::new("Callback")
-                .arg("Post")
-                .arg("Headers")
-                .arg("Reply"),
-        )
+            // LaTeXBodyOptions.callback {{{
+            ArgGroup::new("LaTeXBodyOptions.callback")
+                .arg("LaTeXBodyOptions.callback.post")
+                .arg("LaTeXBodyOptions.callback.headers")
+                .arg("LaTeXBodyOptions.callback.reply"),
+        ) //}}}
         .arg(
-            Arg::new("Post")
+            // LaTeXBodyOptions.callback.post {{{
+            Arg::new("LaTeXBodyOptions.callback.post")
                 .long("post")
                 .about("URL where to post callback")
                 .value_name("URL"),
-        )
+        ) //}}}
         // TODO: Make custom verification for having even number of arguments <29-05-21, kunzaatko> //
         // TODO: Make parsing of $date and $image and so on for the strings in headers <29-05-21, kunzaatko> //
         // TODO: Use custom delimiters for the headers using KEY : VALUE <29-05-21, kunzaatko> //
         .arg(
-            Arg::new("Headers")
+            // LaTeXBodyOptions.callback.headers {{{
+            Arg::new("LaTeXBodyOptions.callback.headers")
                 .long("headers")
-                .about("Key value pairs of headers to make callback post")
+                .about("key value pairs of headers to make callback post")
                 .multiple(true)
                 .value_names(&["KEY", "VALUE"]),
-        )
+        ) //}}}
         // TODO: What are the possible values for this <29-05-21, kunzaatko> //
         .arg(
-            Arg::new("Reply")
+            // LaTeXBodyOptions.callback.reply {{{
+            Arg::new("LaTeXBodyOptions.callback.reply")
                 .long("reply")
-                .about("Sets of values of `reply` field for callback object")
+                .about("values for `reply` field of callback object")
                 .multiple(true)
                 .value_name("VALUE"),
-        )
+        ) //}}}
         .arg(
-            Arg::new("Include Detected Alphabets")
+            // LaTeXBodyOptions.include_detected_alphabets {{{
+            Arg::new("LaTeXBodyOptions.include_detected_alphabets")
                 .long("include_detected_alphabets")
                 .about("Detected alphabets in the response"),
-        );
-
-    // }}}
+        ); //}}}
+           // }}}
 
     let args = App::new("MathpixCLI")
         .version(crate_version!())
