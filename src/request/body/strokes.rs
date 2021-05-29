b3ccf5545efbe64a3a782b50bf63272fd30aa06a
Martin Kunz <martinkunz@email.cz>
2021-05-29 16:55:29 +0200

    edit: strokes formats as enum instead of String

--- src/request/body/strokes.rs
+++ src/request/body/strokes.rs
@@ -22,7 +22,7 @@ pub struct StrokesBodyOptions {
     /// Key value object
     pub metadata: Option<MetaData>,
     /// List of formats, one of `text`, `data`, `html`
-    pub formats: Option<Vec<String>>,
+    pub formats: Option<Vec<StrokesFormats>>,
     /// see [DataOptions](https://docs.mathpix.com/?shell#dataoptions-object) section above, specifies outputs for `data` and `html` return fields
     pub data_options: Option<DataOptions>,
 }
@@ -35,5 +35,40 @@ impl Default for StrokesBodyOptions {
             data_options: None,
         }
     }
+}
+//}}}
+
+// StrokesFormats {{{
+/// Format specifications possible for the _strokes_ endpoint
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum StrokesFormats {
+    /// Mathpix markdown formatted text
+    Text,
+    /// HTML rendered from `text` via mathpix-markdown-it
+    Html,
+    /// Data extracte from `html` as specified in the `data_options` request parameter
+    Data,
+}
+// }}}
+
+// TESTS {{{
+#[cfg(test)]
+mod test {
+    use super::StrokesFormats;
+    use serde_json::json;
+
+    #[test]
+    fn serialize_strokes_formats() {
+        //{{{
+        let strokes_formats: Vec<StrokesFormats> = vec![
+            StrokesFormats::Text,
+            StrokesFormats::Html,
+            StrokesFormats::Data,
+        ];
+        let serialized = serde_json::to_value(&strokes_formats).unwrap();
+        let expected = json!(["text", "html", "data"]);
+        assert_eq!(serialized, expected);
+    } //}}}
 }
 //}}}
